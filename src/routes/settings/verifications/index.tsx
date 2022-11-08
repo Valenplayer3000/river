c093a24afed67c0aaebd0f4dad8d64b378a2f39e
Valenplayer3000 <val.perez@cemep.edu.do>
2022-11-08 14:58:05 -0400

    Version 1.8.1

--- src/routes/settings/verifications/index.tsx
+++ src/routes/settings/verifications/index.tsx
@@ -1,4 +1,4 @@
-import {Card, CardHeader, CardActions, CardContent, Chip, Button, Typography, Stack} from "@mui/material"
+import {Card, CardHeader, CardActions, CardContent, Chip, Button, Typography, Stack, Alert} from "@mui/material"
 
 import { Error, CheckBox } from "@mui/icons-material"
 
@@ -8,10 +8,13 @@ export default function Verifications() {
             <Card variant="outlined">
                 <CardHeader title="Welcome to Verification Page!"/>
                 <CardContent>
-                    <Stack direction="row" gap={0.2}>
+                    <Stack direction="row" gap={0.2} sx={{m:2}}>
                         <Typography>Status: </Typography>
                         <Chip sx={{ marginTop: -1, marginLeft: 1 }} color="error" icon={<Error />} label="Not available" />
                     </Stack>
+                    <Alert severity="warning">
+                        Currently Verification System is very early work in progress
+                    </Alert>
                 </CardContent>
                 <CardActions>
                     <Button disabled variant="contained" startIcon={<CheckBox/>}>Verify</Button>
