d75547b88e766cd68913d3c98cbe22549c268ba0
Bloom <Val.perez@cemep.edu.do>
2022-11-28 12:19:15 -0400

    Post-Update v2

--- src/index.tsx
+++ src/index.tsx
@@ -8,36 +8,17 @@ import {
     Navigate,
 } from "react-router-dom";
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-
 import './index.css';
 
-
-import App from './routes/App';
-import Signup from "./routes/signup/index"
-import PostPage from "./routes/post/PostPage"
-import SettingPage from './routes/settings/SettingPage';
-import Profile from './routes/settings/users/profile';
-import PostID from "./routes/post/PostID"
-import About from './routes/settings/misc/About';
-import Discover from './routes/discover';
-import Verifications from './routes/settings/verifications';
-import Donations from './routes/settings/donations';
+import App from './App';
+import Home from './pages/Home';
+import { ConfigProvider } from 'antd';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
-            <Route index element={<Navigate replace to="/post" />} />
-            <Route path="/post" element={<PostPage />} />
-            <Route path="post/:id" element={<PostID />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/setting/" element={<SettingPage />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/discover" element={<Discover />} />
-            <Route path="/verify" element={<Verifications />} />
-            <Route path="/donate" element={<Donations />} />
+            <Route index element={<Navigate replace to="/home" />} />
+            <Route path="/home" element={<Home />} />
         </Route>
     )
 )
@@ -45,18 +26,10 @@ const router = createBrowserRouter(
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-export const theme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-});
-
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline enableColorScheme>
-                <RouterProvider router={router} />
-            </CssBaseline>
-        </ThemeProvider>
+        <ConfigProvider theme={{token: {borderRadius: 6, colorPrimary: '#1677ff',}}}>
+            <RouterProvider router={router} />
+        </ConfigProvider>
     </React.StrictMode>
 );
